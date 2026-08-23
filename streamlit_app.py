390e44eaeda400f89ef25c834d9d2df587659c20
Joe Uong <joeuong@gmail.com>
2026-08-23 16:21:12 -0400

    testing

---
+++ streamlit_app.py
@@ -0,0 +1,32 @@
+import os
+from datetime import date
+import streamlit as st
+import pandas as pd
+
+st.title("Pokedex Tracker")
+
+@st.cache_data(ttl=60)
+def fetch_data():
+    df = pd.read_csv(data_file)
+    return df
+
+data_file = os.path.join(os.getcwd(), "static", "shinypokemon.csv")
+df = fetch_data()
+
+st.dataframe(df)
+
+static = 'shiny&!traded&'
+display = df['id'].tolist()
+new = '' 
+
+for i in display: 
+    s = str(i)
+    new += s
+    new += ','
+
+concat = static +  new
+clean = concat[:-1]
+
+st.text(clean)
+
+st.code(clean, language=None)
