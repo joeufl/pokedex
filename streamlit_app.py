577ca519385177af4a8b77b5fe571f4aab058f07
Joe Uong <joeuong@gmail.com>
2026-08-23 21:09:38 -0400

    updated shiny and lucky

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,27 +10,38 @@ def fetch_data():
     df = pd.read_csv(data_file)
     return df
 
-data_file = os.path.join(os.getcwd(), "static", "shinypokemon.csv")
+data_file = os.path.join(os.getcwd(), "static", "POKEMON_DATA.csv")
 df = fetch_data()
 
-static = 'shiny&!traded&'
-display = df['id'].tolist()
-new = '' 
-
-for i in display: 
+# Shinies
+shinies = df.loc[df['SHINY'] != 'Y', ['ID']]
+shiny_str = ''
+shiny_list = shinies['ID'].to_list()
+for i in shiny_list: 
     s = str(i)
-    new += s
-    new += ','
+    shiny_str += s
+    shiny_str += ','
+shiny_base = 'shiny&!traded&'
+shiny_concat = shiny_base + shiny_str
+clean_shinies = shiny_concat[:-1]
 
-concat = static +  new
-clean = concat[:-1]
+# Lucky
+lucky = df.loc[df['LUCKY'] != 'Y', ['ID']]
+lucky_str = ''
+lucky_list = lucky['ID'].to_list()
+for i in lucky_list: 
+    s = str(i)
+    lucky_str += s
+    lucky_str += ','
+lucky_base = '!traded&'
+lucky_concat = lucky_base + lucky_str
+clean_lucky = lucky_concat[:-1]
 
+#print
 st.subheader('Tap/Hover over a section below and copy')
 
-
 st.write('Shiny Pokemon')
-st.code(clean, language=None)
+st.code(clean_shinies, language=None)
 
-st.write('Lucky Pokemon (placeholder)')
-
-st.code(clean, language=None)
+st.write('Lucky Pokemon')
+st.code(clean_lucky, language=None)
